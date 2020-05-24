086b1b0acebc167ca99e8970bc8f98700f1777db
Frank Liardet <frank.liardet@gmail.com>
2020-05-24 14:34:46 +1000

    Updated content for 2020

--- src/components/JobHistory.js
+++ src/components/JobHistory.js
@@ -37,33 +37,95 @@ function JobHistory(props) {
     <div className={classes.root}>
       <SectionTitle text='Job History' />
       <Grid container spacing={40}>
-        <Grid item xs={12} >
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit" className={classes.jobPosition}>Head of Product</Typography>
-            <Typography variant="h6" color="inherit" align='right' className={classes.jobDate}>Jun 2017 - Present</Typography>
-            <Typography variant="h6" color="inherit" className={classes.jobCompany}>WooBoard (a REFFIND company)</Typography>
-            <p className={classes.jobDescription}>WooBoard is an employee recognition platform designed to assist companies improve their internal culture. WooBoard was wholly acquired by REFFIND (who I was working for at the time) in late 2015.</p>
+            <Typography variant='h5' color='inherit' className={classes.jobPosition}>
+              Full Stack Developer
+            </Typography>
+            <Typography variant='h6' color='inherit' align='right' className={classes.jobDate}>
+              Jul 2019 - Present
+            </Typography>
+            <Typography variant='h6' color='inherit' className={classes.jobCompany}>
+              ELMO Cloud HR & Payroll
+            </Typography>
+            <p className={classes.jobDescription}>
+              ELMO provides a comprehensive suite of cloud HR, payroll and rostering / time & attendance software
+              solutions that can be accessed via a single dashboard and customised for any organisation
+            </p>
+            <p className={classes.jobDescription}>Responsibilities:</p>
+            <ul>
+              <li>Full stack development using Node, React, Typescript, SQL, Mongo, and microservices architecture</li>
+              <li>Ensuring code is developed in a modular way, and backed by unit tests</li>
+              <li>Assisting with Product Management and BA duties when required</li>
+            </ul>
+            <p className={classes.jobDescription}>Achievements:</p>
+            <ul>
+              <li>
+                Playing my part in helping to build a suite of internal tools to improve efficiencies across all ELMO
+                departments
+              </li>
+
+              <li>Coming 2nd in ELMO's annual Hackathon, building a prototype for publising release notes</li>
+              <li>Learning Typescript, and test writing skills</li>
+            </ul>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant='h5' color='inherit' className={classes.jobPosition}>
+              Head of Product
+            </Typography>
+            <Typography variant='h6' color='inherit' align='right' className={classes.jobDate}>
+              Jun 2017 - Jun 2019
+            </Typography>
+            <Typography variant='h6' color='inherit' className={classes.jobCompany}>
+              WooBoard (a REFFIND company)
+            </Typography>
+            <p className={classes.jobDescription}>
+              WooBoard is an employee recognition platform designed to assist companies improve their internal culture.
+              WooBoard was wholly acquired by REFFIND (who I was working for at the time) in late 2015.
+            </p>
             <p className={classes.jobDescription}>Responsibilities:</p>
             <ul>
               <li>Overseeing a rebuild of the entire platform using new technology</li>
               <li>A full rebrand, including redesigning all user interfaces</li>
               <li>Managing a small development team of two</li>
-              <li>Assisting in the architectural decision making process, in collaboration with the Lead Developer and contract CTO</li>
+              <li>
+                Assisting in the architectural decision making process, in collaboration with the Lead Developer and
+                contract CTO
+              </li>
             </ul>
             <p className={classes.jobDescription}>Achievements:</p>
             <ul>
               <li>Delivering the new platform in a timely fashion</li>
-              <li>Coordinating the migration process to the new platform for all customers (including database migration)</li>
-              <li>I also personally designed and built the new marketing website: <a href='https://wooboard.com' target='_blank' rel='noopener noreferrer'>wooboard.com</a></li>
+              <li>
+                Coordinating the migration process to the new platform for all customers (including database migration)
+              </li>
+              <li>
+                I also personally designed and built the new marketing website:{' '}
+                <a href='https://wooboard.com' target='_blank' rel='noopener noreferrer'>
+                  wooboard.com
+                </a>
+              </li>
             </ul>
           </Paper>
         </Grid>
-        <Grid item xs={12} >
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit" className={classes.jobPosition}>Tech Support Manager</Typography>
-            <Typography variant="h6" color="inherit" align='right' className={classes.jobDate}>Mar 2015 - May 2017</Typography>
-            <Typography variant="h6" color="inherit" className={classes.jobCompany}>REFFIND Ltd</Typography>
-            <p className={classes.jobDescription}>REFFIND is an ASX listed startup in the HR / Customer Loyalty space. Initially the company’s only offering was an employee referral app, but then WooBoard was also acquired. As one of the first employees at REFFIND my role was to establish and lead the area of Tech Support.</p>
+            <Typography variant='h5' color='inherit' className={classes.jobPosition}>
+              Tech Support Manager
+            </Typography>
+            <Typography variant='h6' color='inherit' align='right' className={classes.jobDate}>
+              Mar 2015 - May 2017
+            </Typography>
+            <Typography variant='h6' color='inherit' className={classes.jobCompany}>
+              REFFIND Ltd
+            </Typography>
+            <p className={classes.jobDescription}>
+              REFFIND is an ASX listed startup in the HR / Customer Loyalty space. Initially the company’s only offering
+              was an employee referral app, but then WooBoard was also acquired. As one of the first employees at
+              REFFIND my role was to establish and lead the area of Tech Support.
+            </p>
             <p className={classes.jobDescription}>Responsibilities:</p>
             <ul>
               <li>Troubleshooting customer issues and following them through to resolution</li>
@@ -75,32 +137,64 @@ function JobHistory(props) {
             <p className={classes.jobDescription}>Achievements:</p>
             <ul>
               <li>Learning SQL to diagnose support issues and produce customer reports</li>
-              <li>Developing an overall knowledge of our software platforms and service offerings (end to end) to such an extent that I became the person that others turned to when there was a problem they couldn't resolve</li>
+              <li>
+                Developing an overall knowledge of our software platforms and service offerings (end to end) to such an
+                extent that I became the person that others turned to when there was a problem they couldn't resolve
+              </li>
             </ul>
           </Paper>
         </Grid>
-        <Grid item xs={12} >
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit" className={classes.jobPosition}>Freelance Web Developer</Typography>
-            <Typography variant="h6" color="inherit" align='right' className={classes.jobDate}>Aug 2014 - Feb 2015</Typography>
-            <Typography variant="h6" color="inherit" className={classes.jobCompany}>Emotive Websites</Typography>
-            <p className={classes.jobDescription}>Emotive Websites was my own freelance web design business - a short experiment where I learned a great deal, but ultimately realised that I didn't enjoy solely building simple websites. Hence I returned to the faster pace and broader horizons of the SaaS space.</p>
+            <Typography variant='h5' color='inherit' className={classes.jobPosition}>
+              Freelance Web Developer
+            </Typography>
+            <Typography variant='h6' color='inherit' align='right' className={classes.jobDate}>
+              Aug 2014 - Feb 2015
+            </Typography>
+            <Typography variant='h6' color='inherit' className={classes.jobCompany}>
+              Emotive Websites
+            </Typography>
+            <p className={classes.jobDescription}>
+              Emotive Websites was my own freelance web design business - a short experiment where I learned a great
+              deal, but ultimately realised that I didn't enjoy solely building simple websites. Hence I returned to the
+              faster pace and broader horizons of the SaaS space.
+            </p>
           </Paper>
         </Grid>
-        <Grid item xs={12} >
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit" className={classes.jobPosition}>Full Time Student</Typography>
-            <Typography variant="h6" color="inherit" align='right' className={classes.jobDate}>Feb - Jul 2014</Typography>
-            <Typography variant="h6" color="inherit" className={classes.jobCompany}>TAFE NSW - Cert 4 in Web Based Technologies (Design & Development)</Typography>
-            <p className={classes.jobDescription}>After studying in the evenings throughout 2013, I completed my qualification full-time during this period.</p>
+            <Typography variant='h5' color='inherit' className={classes.jobPosition}>
+              Full Time Student
+            </Typography>
+            <Typography variant='h6' color='inherit' align='right' className={classes.jobDate}>
+              Feb - Jul 2014
+            </Typography>
+            <Typography variant='h6' color='inherit' className={classes.jobCompany}>
+              TAFE NSW - Cert 4 in Web Based Technologies (Design & Development)
+            </Typography>
+            <p className={classes.jobDescription}>
+              After studying in the evenings throughout 2013, I completed my qualification full-time during this period.
+            </p>
           </Paper>
         </Grid>
-        <Grid item xs={12} >
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit" className={classes.jobPosition}>Tech Support Manager</Typography>
-            <Typography variant="h6" color="inherit" align='right' className={classes.jobDate}>Sep 2012 - Feb 2014</Typography>
-            <Typography variant="h6" color="inherit" className={classes.jobCompany}>Connect2Field</Typography>
-            <p className={classes.jobDescription}>Connect2Field (now Verizon Connect) provides job management and scheduling software for businesses running a team of fieldworkers.</p>
+            <Typography variant='h5' color='inherit' className={classes.jobPosition}>
+              Tech Support Manager
+            </Typography>
+            <Typography variant='h6' color='inherit' align='right' className={classes.jobDate}>
+              Sep 2012 - Feb 2014
+            </Typography>
+            <Typography variant='h6' color='inherit' className={classes.jobCompany}>
+              Connect2Field
+            </Typography>
+            <p className={classes.jobDescription}>
+              Connect2Field (now Verizon Connect) provides job management and scheduling software for businesses running
+              a team of fieldworkers. After joining the company, we grew quickly and I was soon recruiting and leading
+              the support team. Connect2Field was then acquired by a larger US firm and I experienced all that came with
+              that process.
+            </p>
             <p className={classes.jobDescription}>Responsibilities:</p>
             <ul>
               <li>Troubleshooting customer issues and following them through to resolution</li>
@@ -112,21 +206,33 @@ function JobHistory(props) {
             <p className={classes.jobDescription}>Achievements:</p>
             <ul>
               <li>Recruiting, training and managing a support team of four</li>
-              <li>Managing the redesign of 4 accounting connectors (API integrated applications used to transfer data to MYOB, QuickBooks, Xero and Saasu)</li>
+              <li>
+                Managing the redesign of 4 accounting connectors (API integrated applications used to transfer data to
+                MYOB, QuickBooks, Xero and Saasu)
+              </li>
             </ul>
           </Paper>
         </Grid>
-        <Grid item xs={12} >
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit" className={classes.jobPosition}>Prior Career in Fitness Coaching</Typography>
-            <Typography variant="h6" color="inherit" align='right' className={classes.jobDate}>1996 - 2012</Typography>
-            <Typography variant="h6" color="inherit" className={classes.jobCompany}>Personal Trainer and Strength & Conditioning Coach</Typography>
-            <p className={classes.jobDescription}>Prior to working in Tech, I worked in the fitness industry where I trained elite athletes, and also owned and operated a boutique personal training studio.</p>
+            <Typography variant='h5' color='inherit' className={classes.jobPosition}>
+              Prior Career in Fitness Coaching
+            </Typography>
+            <Typography variant='h6' color='inherit' align='right' className={classes.jobDate}>
+              Prior to 2012
+            </Typography>
+            <Typography variant='h6' color='inherit' className={classes.jobCompany}>
+              Personal Trainer and Strength & Conditioning Coach
+            </Typography>
+            <p className={classes.jobDescription}>
+              Prior to working in Tech, I worked in the fitness industry where I trained elite athletes, and also owned
+              and operated a boutique personal training studio.
+            </p>
           </Paper>
         </Grid>
       </Grid>
     </div>
-  )
+  );
 }
 
 export default withStyles(styles)(JobHistory);
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -19,7 +19,7 @@ const styles = {
     marginBottom: 50,
     '& p': {
       marginBottom: 30,
-    }
+    },
   },
   italics: {
     fontStyle: 'italic',
@@ -32,49 +32,74 @@ function Background(props) {
     <>
       <SectionTitle text='Summary' />
       <div className={classes.initialSummary}>
-        <p>For the past 6 years, whilst working in startups, I've also been learning code and building my own websites and apps. I am now looking to transition into programming full time.</p>
-        <p>My tech career began in tech support, which allowed me to support myself whilst I built up my skill base. My current employer then offered me the opportunity to lead a project to rebuild one of our software platforms (WooBoard) as Head of Product. It was an amazing experience. In that role I managed two developers and thoroughly enjoyed delving deeper and more strategically into the tech.</p>
-        <p>I’m now at a stage where I would like to code full-time. Ultimately I would like to be a CTO and this is another step along that path.</p>
+        <p>
+          My journey has been unique, and I believe that's a strength. My tech career began in tech support, which
+          allowed me to support myself whilst building up my skill base. During that time I began learning HTML, CSS and
+          PHP at nights and on the weekend, and built websites and simple web apps.
+        </p>
+        <p>
+          I then had the opportunity to lead a project to rebuild a software platform (WooBoard) as Head of Product. It
+          was an amazing experience. In that role I managed two developers and thoroughly enjoyed delving deeper and
+          more strategically into the tech, including an involvement in architectural decision making.
+        </p>
+        <p>
+          Ultimately though, I've always been drawn to the code, and I finally reached my goal of becoming a full stack
+          software developer in my current role.
+        </p>
       </div>
       <Grid container spacing={40}>
         <Grid className={classes.gridItem} item xs={12} md={6}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit">My Coding Journey</Typography>
-            <p>Like many others, I started with HTML, CSS and PHP, where I began building websites using WordPress themes and also created my first simple web app.</p>
-            <p>From there I completed a 3 month Ruby on Rails night course at CoderFactory, where I built "Feedsta" (see portfolio) which integrates with the Instagram API.</p>
-            <p>More recently I've been learning React, Node and Mongo. To help me learn I created "Spot Quiz" where I can pre-enter questions and test myself regularly.</p>
-            <p>Fun fact: I also built this website using React.</p>
+            <Typography variant='h5' color='inherit'>
+              My Coding Journey
+            </Typography>
+            <p>
+              Like many others, I started with HTML, CSS and PHP, where I began building websites using WordPress themes
+              and also created my first simple web app.
+            </p>
+            <p>
+              From there I completed a 3 month Ruby on Rails night course at CoderFactory, where I built "Feedsta" (see
+              portfolio) which integrates with the Instagram API.
+            </p>
+            <p>
+              I have had commercial experience with MySQL and PostgreSQL in my roles at REFFIND and WooBoard where I
+              used them to diagnose customer issues, find bugs and run reports.
+            </p>
+            <p>
+              More recently in my role at ELMO, I've been working with React, Node and MongoDB. I also have a modest
+              level of experience with AWS, having had exposure to it whilst working at WooBoard, and also hosting my
+              own apps using Elastic Beanstalk and S3.
+            </p>
           </Paper>
         </Grid>
-        <Grid className={classes.gridItem} item xs={12} md={6} >
+        <Grid className={classes.gridItem} item xs={12} md={6}>
           <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit">Career Summary</Typography>
-            <p>My first startup role was at Connect2Field providing tech support for a SaaS product in the fieldworker space. We grew quickly and I was soon recruiting and leading the support team. In the evenings I studied web development.</p>
-            <p>Connect2Field was then acquired by a larger US firm and I experienced all that came with that process.</p>
-            <p>Following a brief stint as a freelance web developer, I joined REFFIND as Tech Support Manager. We listed on the ASX four months later and went on a rollercoaster ride. Later I was given the opportunity to lead a project to rebuild one of our software platforms (WooBoard) as Head of Product.</p>
-          </Paper>
-        </Grid>
-        <Grid className={classes.gridItem} item xs={12} md={6} >
-          <Paper className={classes.paper}>
-            <Typography variant="h5" color="inherit">Programming Skills</Typography>
-            <p>I have built my own apps using HTML5, CSS3, Sass, Bootstrap, JSON, PHP, Ruby on Rails, Javascript, React and Node - see Portfolio. </p>
-            <p>I've hosted these apps on Heroku and AWS, and also used Photoshop and Illustrator to create assets.</p>
-            <p>I have strong SQL skills (both  MySQL and PostgreSQL) having used them on a daily basis in my previous role to diagnose customer issues, find bugs and run reports.</p>
-            <p>Manual software QA testing (using the UI) is also an area I've had a lot of experience in.</p>
-          </Paper>
-        </Grid>
-        <Grid className={classes.gridItem} item xs={12} md={6} >
-          <Paper className={classes.paper}>
-          <Typography variant="h5" color="inherit">Additional Skills</Typography>
-          <p></p>
-          <p>Although I'm seeking a developer role, that doesn't mean you can't take advantage of my other skills and experiences when required.</p>
-          <p>I'm happy to share my knowledge in relation to UX / UI, design, feature scoping and product strategy.</p>
-          <p>As someone who has worked across different business areas, I bring a circumspect view and can explain complex concepts in clear language to non-technical stakeholders</p>
+            <Typography variant='h5' color='inherit'>
+              How Diversity Helps
+            </Typography>
+            <p>
+              Over the past couple of decades, the world has pursued specialisation in the quest for success. But the
+              mood is changing. It is now recognised that a diverse range of experiences can help individuals join the
+              dots in a more wholistic sense to produce better outcomes.
+            </p>
+            <p>
+              My time in tech support has given me empathy for the customer. It has also taught me how to adjust my
+              language when speaking with non-technical people, to ensure understanding.
+            </p>
+            <p>
+              My time in product management has sharpened my eye for design and UX. When this is coupled with my tech
+              support background, I now have a strong sense for the types of things that might trip up users on a page,
+              and how to guard against it.
+            </p>
+            <p>
+              As a result I am a developer who can place himself squarely in the shoes of the user, which is a
+              perspective that many developers struggle with.
+            </p>
           </Paper>
         </Grid>
       </Grid>
     </>
-  )
+  );
 }
 
 export default withStyles(styles)(Background);
